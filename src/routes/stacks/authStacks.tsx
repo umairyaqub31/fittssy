ecd459ea78fe7839da9db6a6ffffb1a2a6551555
salimqureshi147 <salimasim234@gmail.comm>
2024-02-09 15:05:41 +0500

    some new Components including CustomInput

--- src/routes/stacks/authStacks.tsx
+++ src/routes/stacks/authStacks.tsx
@@ -7,8 +7,8 @@ const Stack = createStackNavigator();
 const AuthStack = ({route}: any) => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="GetStarted" component={GetStarted} />
       <Stack.Screen name="GetInformation" component={GetInformation} />
-      {/* <Stack.Screen name="GetStarted" component={GetStarted} /> */}
       <Stack.Screen name="Login" component={Login} />
     </Stack.Navigator>
   );
