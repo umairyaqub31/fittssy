3f712534210dc2102d6d5a846be27d356c1026c2
berryboostbb <berryboostbb@gmail.com>
2024-02-13 22:59:55 +0500

    change

--- src/routes/stacks/authStacks.tsx
+++ src/routes/stacks/authStacks.tsx
@@ -14,12 +14,12 @@ const Stack = createStackNavigator();
 const AuthStack = ({route}: any) => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
-      {/* <Stack.Screen name="GetStarted" component={GetStarted} />
-      <Stack.Screen name="GetInformation" component={GetInformation} /> */}
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="GetStarted" component={GetStarted} />
+      <Stack.Screen name="GetInformation" component={GetInformation} />
       <Stack.Screen name="Register" component={Register} />
       <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
       <Stack.Screen name="VerificatioOTP" component={VerificatioOTP} />
-      <Stack.Screen name="Login" component={Login} />
     </Stack.Navigator>
   );
 };
