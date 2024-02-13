17bcf6c6937af6cfb4fcd36718c024b81e455fb8
salimqureshi147 <salimasim234@gmail.comm>
2024-02-13 23:44:17 +0500

    update

--- src/screens/homeTab/home/index.tsx
+++ src/screens/homeTab/home/index.tsx
@@ -1,12 +1,135 @@
-import React from 'react';
-import {Text} from 'react-native';
-import {Wrapper} from '@components';
+import React, {useState} from 'react';
+import {
+  FlatList,
+  Image,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {RF} from '@theme';
+import {Text, WorkoutItem} from '@components';
+import {useTheme} from '@react-navigation/native';
+import LinearGradient from 'react-native-linear-gradient';
+import useStyles from './styles';
+import {back, exercise, next} from '@assets';
+import {navigate} from '@services';
 
 const Home = () => {
+  const theme: any = useTheme();
+  const colors = theme.colors;
+  const styles = useStyles(colors);
+  const [selectedTab, setSelectedTab] = useState('exerciseOfTheDay');
+  const [selected, setSelected] = useState('');
+
+  const [selectedItemIndex, setSelectedItemIndex] = useState(0);
+  const handlePressWorkout = () => {
+    // navigate('WorkoutDetail', {selectedDate: selected});
+  };
   return (
-    <Wrapper>
-      <Text>Home</Text>
-    </Wrapper>
+    <View style={{marginHorizontal: RF(20)}}>
+      <View style={styles.MainContainer}>
+        <View style={styles.ImageView}>
+          <Image source={back} style={styles.ImageStyle} />
+        </View>
+        <View style={styles.rowStyles}>
+          <Text size={16} bold color={'rgba(13, 13, 13, 1)'}>
+            Today
+          </Text>
+          <Text size={14} regular color={'rgba(13, 13, 13, 1)'}>
+            (Wed 27/11/23)
+          </Text>
+        </View>
+        <View style={styles.ImageView}>
+          <Image source={next} style={styles.ImageStyle} />
+        </View>
+      </View>
+      <View style={{width: RF(170), marginTop: RF(20), gap: RF(4)}}>
+        <Text size={22} regular color={'rgba(13, 13, 13, 1)'}>
+          Welcome back
+        </Text>
+        <Text size={24} regular color={'rgba(40, 204, 158, 1)'}>
+          Sarah!
+        </Text>
+      </View>
+      <View style={styles.TabStyle}>
+        <TouchableOpacity
+          onPress={() => setSelectedTab('exerciseOfTheDay')}
+          style={{
+            borderBottomWidth: selectedTab === 'exerciseOfTheDay' ? 2 : 0.5,
+            width: '50%',
+            padding: RF(8),
+            borderColor:
+              selectedTab === 'exerciseOfTheDay'
+                ? 'rgba(54, 216, 170, 1)'
+                : 'rgba(174, 173, 173, 1)',
+          }}>
+          <Text
+            color={
+              selectedTab === 'exerciseOfTheDay'
+                ? 'rgba(54, 216, 170, 1)'
+                : 'rgba(174, 173, 173, 1)'
+            }
+            size={14}
+            medium>
+            Exercise of the day
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => setSelectedTab('previousExercises')}
+          style={{
+            borderBottomWidth: selectedTab === 'previousExercises' ? 2 : 0.5,
+            width: '50%',
+            padding: RF(8),
+            borderColor:
+              selectedTab === 'previousExercises'
+                ? 'rgba(54, 216, 170, 1)'
+                : 'rgba(174, 173, 173, 1)',
+          }}>
+          <Text
+            color={
+              selectedTab === 'previousExercises'
+                ? 'rgba(54, 216, 170, 1)'
+                : 'rgba(174, 173, 173, 1)'
+            }
+            size={14}
+            medium>
+            Previous Exercises
+          </Text>
+        </TouchableOpacity>
+      </View>
+      {selectedTab === 'exerciseOfTheDay' ? (
+        <LinearGradient
+          colors={['#fff', '#fff']}
+          style={{width: '100%', height: '55%', marginTop: RF(20)}}>
+          <ImageBackground
+            source={exercise}
+            style={styles.ImageBackgroundStyle}>
+            <Text size={18} medium color={'rgba(209, 209, 209, 1)'}>
+              Workout log is empty
+            </Text>
+          </ImageBackground>
+        </LinearGradient>
+      ) : (
+        <View>
+          <FlatList
+            showsVerticalScrollIndicator={false}
+            data={[1, 2, 3, 4, 5]}
+            renderItem={({item, index}) => (
+              <WorkoutItem
+                selectedItemIndex={selectedItemIndex}
+                setSelectedItemIndex={setSelectedItemIndex}
+                colors={colors}
+                item={item}
+                index={index}
+                styles={styles}
+                onPress={handlePressWorkout}
+              />
+            )}
+          />
+        </View>
+      )}
+    </View>
   );
 };
 
---
+++ src/shared/components/workoutItem/index.tsx
@@ -0,0 +1,224 @@
+import {
+  FlatList,
+  ImageBackground,
+  Pressable,
+  StyleSheet,
+  View,
+} from 'react-native';
+import React from 'react';
+import {gradientFlex, unSelect} from '@assets';
+import Text from '../text';
+import {RF} from '@theme';
+
+const WorkoutItem = (props: any) => {
+  const {colors, item, index, selectedItemIndex, onPress} = props;
+  const styles = useStyles(colors);
+  return (
+    <Pressable style={styles.listItemContainer} onPress={onPress}>
+      <ImageBackground
+        source={index == selectedItemIndex ? gradientFlex : unSelect}
+        imageStyle={{
+          height: '100%',
+          resizeMode: 'stretch',
+        }}
+        style={{
+          height: '100%',
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          //   backgroundColor: colors.card,
+          borderRadius: RF(16),
+        }}>
+        <View style={[styles.row, {marginBottom: 4}]}>
+          <Text
+            style={{
+              color: index == selectedItemIndex ? colors.primary : colors.text,
+            }}
+            size={12}
+            thin>
+            Training:{' '}
+          </Text>
+          <Text
+            style={{
+              color: index == selectedItemIndex ? colors.primary : colors.text,
+            }}
+            size={14}
+            medium>
+            00:40:08{' '}
+          </Text>
+          <View
+            style={[
+              styles.verticleLine,
+              {
+                backgroundColor:
+                  index == selectedItemIndex ? colors.primary : colors.grayText,
+              },
+            ]}
+          />
+
+          <Text
+            style={{
+              color: index == selectedItemIndex ? colors.primary : colors.text,
+            }}
+            size={12}
+            thin>
+            {' '}
+            Actual:{' '}
+          </Text>
+          <Text
+            style={{
+              color: index == selectedItemIndex ? colors.primary : colors.text,
+            }}
+            size={14}
+            medium>
+            00:13:12{' '}
+          </Text>
+          <View
+            style={[
+              styles.verticleLine,
+              {
+                backgroundColor:
+                  index == selectedItemIndex ? colors.primary : colors.grayText,
+              },
+            ]}
+          />
+          <Text
+            style={{
+              color: index == selectedItemIndex ? colors.primary : colors.text,
+            }}
+            size={12}
+            thin>
+            {' '}
+            Rest:{' '}
+          </Text>
+          <Text
+            style={{
+              color: index == selectedItemIndex ? colors.primary : colors.text,
+            }}
+            size={14}
+            medium>
+            00:27:59{' '}
+          </Text>
+        </View>
+
+        <View style={[styles.row]}>
+          <Text
+            style={{
+              color: index == selectedItemIndex ? colors.primary : colors.text,
+            }}
+            size={12}
+            thin>
+            Idle:{' '}
+          </Text>
+          <Text
+            style={{
+              color: index == selectedItemIndex ? colors.primary : colors.text,
+            }}
+            size={14}
+            medium>
+            00:00:00{' '}
+          </Text>
+          <View
+            style={[
+              styles.verticleLine,
+              {
+                backgroundColor:
+                  index == selectedItemIndex ? colors.primary : colors.grayText,
+              },
+            ]}
+          />
+
+          <Text
+            style={{
+              color: index == selectedItemIndex ? colors.primary : colors.text,
+            }}
+            size={12}
+            thin>
+            {' '}
+            Complete:{' '}
+          </Text>
+          <Text
+            style={{
+              color: index == selectedItemIndex ? colors.primary : colors.text,
+            }}
+            size={14}
+            medium>
+            8{' '}
+          </Text>
+          <View
+            style={[
+              styles.verticleLine,
+              {
+                backgroundColor:
+                  index == selectedItemIndex ? colors.primary : colors.grayText,
+              },
+            ]}
+          />
+          <Text
+            style={{
+              color: index == selectedItemIndex ? colors.primary : colors.text,
+            }}
+            size={12}
+            thin>
+            {' '}
+            Weight:{' '}
+          </Text>
+          <Text
+            style={{
+              color: index == selectedItemIndex ? colors.primary : colors.text,
+            }}
+            size={14}
+            medium>
+            6896.0 kg{' '}
+          </Text>
+        </View>
+
+        <View style={styles.lineStyle} />
+
+        <Text
+          style={{
+            color: index == selectedItemIndex ? colors.primary : colors.text,
+          }}
+          size={18}
+          semiBold>
+          Workout 2
+        </Text>
+      </ImageBackground>
+    </Pressable>
+  );
+};
+
+export default WorkoutItem;
+
+const useStyles = (colors: any) =>
+  StyleSheet.create({
+    footerDot: {
+      height: 12,
+      width: 12,
+      borderRadius: 6,
+      marginRight: 6,
+    },
+    footerText: {
+      color: colors.grayText,
+    },
+    dateText: {
+      color: colors.text,
+    },
+    listItemContainer: {
+      height: RF(105),
+      backgroundColor: colors.card,
+      marginBottom: 20,
+      borderRadius: 16,
+    },
+    verticleLine: {
+      height: '65%',
+      width: 1,
+      //   backgroundColor: '#909090',
+    },
+    lineStyle: {
+      borderBottomColor: 'gray',
+      borderBottomWidth: StyleSheet.hairlineWidth,
+      marginVertical: 10,
+    },
+    row: {flexDirection: 'row', alignItems: 'center'},
+  });
